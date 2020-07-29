9cdb10d4695d17729305710966f153a71c205518
Jerry Chen <jcdsta@gmail.com>
2020-07-30 09:50:19 +1000

    Restsharp: Post sample

--- APITestings/RSDemo1/RSDemo1/UnitTest1.cs
+++ APITestings/RSDemo1/RSDemo1/UnitTest1.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using RestSharp;
 using RestSharp.Serialization.Json;
+using RSDemo1.Model;
 using Utf8Json;
 
 namespace RSDemo1
@@ -57,5 +58,50 @@ namespace RSDemo1
             Assert.That(obs["author"].ToString(), Is.EqualTo("Karthik KK"), "Author is not correct");
 
         }
+
+
+        [Test]
+        public void PostWithAnonymousBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}/profile", Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new { name = "Raj" });
+
+            request.AddUrlSegment("postid", 1);
+            var response = client.Execute(request);
+            var des = new JsonDeserializer();
+            var output = des.Deserialize<Dictionary<string, string>>(response);
+            var result = output["name"];
+            Assert.That(result, Is.EqualTo("Raj"), "Author is not correct");
+           
+
+        }
+
+
+
+        [Test]
+        public void PostWithTypeClassBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts", Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Post() { id = "17", author = "Execute Automation", title = "RestSharp demo course" });
+
+            var response = client.Execute<Post>(request);
+
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["author"];
+
+            Assert.That(response.Data.author, Is.EqualTo("Execute Automation"), "Author is not correct");
+
+        }
+
+
     }
 }
